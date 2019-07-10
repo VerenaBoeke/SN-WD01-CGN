970c9347d0af25e714f0ea8b2b9158df8cf8d336
ChefkochUX <vboeke@gmail.com>
2019-07-10 16:39:36 +0200

    Buzz fix :-)

--- Homework-09/fizzbuzz.py
+++ Homework-09/fizzbuzz.py
@@ -10,21 +10,23 @@ If the number is divisible with 5, it prints "buzz". If it's divisible with both
 [ ] If the number is divisiable with 3 - Print "fizz"
 [ ] If the number is divisable with 5 - Print "buzz"
 [ ] If the number divisable with 3 _and_ 5 - Print "fizzbuzz"
+
+[ ] Prüfen ob die Zahl sich zwischen 1 und 100 befindet
+[ ] Prüfen ob die Eingabe eine Zahl ist
 """
 
 
 userNumber = int(input("Let`s play a game! Gimme a number between 1 and 100 and i tell you which one is a Fizz, a Buzz or a FizzBuzz: "))
 
-for number in range(1, userNumber + 1):
-    if int(number) % 3 == 0 and int(number) % 5 == 0:
+for number in range(1, int(userNumber) + 1):
+    if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz!!!!")
     elif number % 3 == 0:
         print("fizz")
     elif number % 5 == 0:
         print("buzz")
-    elif number == userNumber:
-        print("Fertig!")
-        break
     else:
         print(number)
+
+print("Fertig!")
 
